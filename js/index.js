84e9c233dc02ffb2e9f723bb94abf9cb925a6cd0
miksezea <mikk289e@edu.zealand.dk>
2022-10-26 12:53:39 +0200

    s

--- js/index.js
+++ js/index.js
@@ -7,18 +7,19 @@ console.log("My Shoppinglist")
 // myElement.removeAttribute("class")
 // myElement.setAttribute("id","1000")
 
-let list =document.getElementsByTagName("li")
+// let list = document.getElementsByTagName("li")
 // console.log(list)
 
-let newLielement = document.createElement("li")
+// let newLielement = document.createElement("li")
 
-let newTextelement = document.createTextNode("Mælk")
-newLielement.setAttribute("class","healthy")
-newLielement.setAttribute("id","4")
-newLielement.append(newTextelement)
+// let newTextelement = document.createTextNode("Mælk")
+// newLielement.setAttribute("class","healthy")
+// newLielement.setAttribute("id","4")
+// newLielement.append(newTextelement)
 
-document.getElementById("list").append(newLielement)
+// document.getElementById("list").append(newLielement)
 
+/*
 for (let index = 0; index < list.length; index++) {
     const element = list[index];
 
@@ -28,4 +29,25 @@ for (let index = 0; index < list.length; index++) {
         // element.setAttribute("class","unhealthy")
         element.removeAttribute("class")
     }
+}
+*/
+
+document.getElementById("addButton").addEventListener('click',addProducts)
+let elementInput = document.getElementById('newProducts')
+let descriptionChecked = document.querySelector('[name=description]:checked')
+
+function addProducts(){
+    console.log("Add products function used");
+    let inputText = (elementInput).value;
+    let radioButton = (descriptionChecked).value;
+    console.log("The input text is: " + inputText + ". The selected value is: " + radioButton + ".");
+
+    let list = document.getElementById("list");
+    let newLi = document.createElement('li');
+    let newText = document.createTextNode(inputText);
+
+    newLi.appendChild(newText);
+    newLi.setAttribute('class',radioButton);
+
+    list.appendChild(newLi);
 }
