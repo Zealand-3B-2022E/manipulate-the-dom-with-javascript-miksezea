7f73de2438ee5605a67beb2730f0b5a2f91cae05
miksezea <mikk289e@edu.zealand.dk>
2022-10-26 09:52:59 +0200

    s

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
 //Write your Javascript code here
-console.log("Shoppinglist")
+console.log("My Shoppinglist")
+
+// let myElement = document.getElementById("3")
+// console.log(myElement)
+// myElement.setAttribute("class","unhealthy")
+// myElement.removeAttribute("class")
+// myElement.setAttribute("id","1000")
+
+let list =document.getElementsByTagName("li")
+// console.log(list)
+
+let newLielement = document.createElement("li")
+
+let newTextelement = document.createTextNode("Mælk")
+newLielement.setAttribute("class","healthy")
+newLielement.setAttribute("id","4")
+newLielement.append(newTextelement)
+
+document.getElementById("list").append(newLielement)
+
+for (let index = 0; index < list.length; index++) {
+    const element = list[index];
+
+    console.log(element)
+
+    if(element.getAttribute("class")=="healthy"){
+        // element.setAttribute("class","unhealthy")
+        element.removeAttribute("class")
+    }
+}
