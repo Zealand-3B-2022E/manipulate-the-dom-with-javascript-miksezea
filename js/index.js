88ad26901b1d78a37c96f665f04e0e581d3b38a7
miksezea <mikk289e@edu.zealand.dk>
2022-10-26 13:08:50 +0200

    næsten færdig

--- js/index.js
+++ js/index.js
@@ -34,20 +34,29 @@ for (let index = 0; index < list.length; index++) {
 
 document.getElementById("addButton").addEventListener('click',addProducts)
 let elementInput = document.getElementById('newProducts')
-let descriptionChecked = document.querySelector('[name=description]:checked')
+//let descriptionChecked = document.querySelector('[name=description]:checked')
+//let descriptions = document.getElementsByName('description')
+let descriptionInput = document.getElementById('productDescription')
 
 function addProducts(){
     console.log("Add products function used");
     let inputText = (elementInput).value;
-    let radioButton = (descriptionChecked).value;
-    console.log("The input text is: " + inputText + ". The selected value is: " + radioButton + ".");
+    let descriptionText = (descriptionInput).value;
+
+    /*
+    let radioButton;
+    if(descriptions.checked)
+    */
+
+    console.log("The input text is: " + inputText + ". The selected value is: " + descriptionText + ".");
 
     let list = document.getElementById("list");
     let newLi = document.createElement('li');
     let newText = document.createTextNode(inputText);
 
     newLi.appendChild(newText);
-    newLi.setAttribute('class',radioButton);
+    //newLi.setAttribute('class',radioButton);
+    newLi.setAttribute('class',descriptionText)
 
     list.appendChild(newLi);
 }
